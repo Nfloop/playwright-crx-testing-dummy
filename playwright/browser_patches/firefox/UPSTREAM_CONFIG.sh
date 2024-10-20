af2ac01ca421ac0732ff0bd0b8a6780138d311aa
Manoj Kumar Jakkula <37655351+jakkulamanojkumar27@users.noreply.github.com>
2024-10-20 12:58:36 +0530

    Delete playwright directory

--- playwright/browser_patches/firefox/UPSTREAM_CONFIG.sh
+++
@@ -1,3 +0,0 @@
-REMOTE_URL="https://github.com/mozilla/gecko-dev"
-BASE_BRANCH="release"
-BASE_REVISION="cf0397e3ba298868fdca53f894da5b0d239dc09e"
--- playwright/browser_patches/webkit/UPSTREAM_CONFIG.sh
+++
@@ -1,3 +0,0 @@
-REMOTE_URL="https://github.com/WebKit/WebKit.git"
-BASE_BRANCH="main"
-BASE_REVISION="f371dbc2bb4292037ed394e2162150a16ef977fc"
