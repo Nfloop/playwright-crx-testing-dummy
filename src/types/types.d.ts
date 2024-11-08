51b2be2860b3952eb6755a18a2817159dbb8cceb
Manoj Kumar Jakkula <37655351+jakkulamanojkumar27@users.noreply.github.com>
2024-11-08 18:55:12 +0530

    Update types.d.ts

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,31 +1,11 @@
-/**
- * Copyright (c) Rui Figueira.
- *
- * Licensed under the Apache License, Version 2.0 (the 'License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import { Page, BrowserContext } from '../../playwright/packages/playwright-core/types/types';
 export * from '../../playwright/packages/playwright-core/types/types';
 
 export interface Crx {
-  /**
-   * @param options
-   */
+  
   start(options?: {
-    /**
-     * Slows down Playwright operations by the specified amount of milliseconds. Useful so that you can see what is going
-     * on.
-     */
+    
     slowMo?: number;
   }): Promise<CrxApplication>;
 }
